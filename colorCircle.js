fc5b2ac569fb2ae0c6b4bcea4f097156749774b1
steliviere <32293736+steliviere@users.noreply.github.com>
2017-11-02 17:15:41 +0900

    Create colorCircle.js

---
+++ colorCircle.js
@@ -0,0 +1,36 @@
+var slider;
+function setup()
+{
+	createCanvas(500,500);
+	noStroke();
+	colorMode(HSB,360,100,100);
+	slider=createSlider(3,36,6);
+	slider.position(10,10);
+}
+function draw()
+{
+	background(255);
+	colorCircle(slider.value());
+}
+function colorCircle(n)
+{
+	var circleRadius=min(width,height)*1/3;
+	var rotateVector=createVector(circleRadius,0);
+	var center=createVector(width/2,height/2);
+	var p=createVector();
+	var i, col;
+	var r=TWO_PI/n;
+	for(i=0;i<n;i++)
+	{
+		col=color(map(i,0,n,0,360),100,100);
+		fill(col);
+		beginShape();
+		vertex(center.x,center.y);
+		p=p5.Vector.add(rotateVector,center);
+		vertex(p.x,p.y);
+		rotateVector.rotate(r);
+		p=p5.Vector.add(rotateVector,center);
+		vertex(p.x,p.y);
+		endShape(CLOSE);
+	}
+}
